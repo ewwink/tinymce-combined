08a96797ab86849843b2b9750dddc721afad43b0
erwin solihin <ewwink@users.noreply.github.com>
2025-12-27 09:38:25 +0000

    add files

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
-# tinymce-combined
-Updated TinyMCE and Plugins Combined into one js file
 
-<img width="931" height="615" alt="TinyMCE Combined" src="https://github.com/user-attachments/assets/f9d3b3f5-b5af-4590-a0e0-b28a0842d3e7" />
+## ewwink/tinymce-combined
+
+TinyMCE and Plugins Combined into one js file, it uses Github Actions to pull and combine the files from
+
+```
+https://www.jsdelivr.com/package/npm/tinymce
+```
+The Actions check new version of TinyMCE every day (daily cronjob)
+
+## Use it
+```html
+<link href="https://cdn.jsdelivr.net/gh/ewwink/tinymce-combined@latest/dist/skins/ui/oxide/content.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/gh/ewwink/tinymce-combined@latest/dist/tinymce-combined.min.js"></script>
+
+or use specific version
+
+<link href="https://cdn.jsdelivr.net/gh/ewwink/tinymce-combined@8.3.0/dist/skins/ui/oxide/content.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/gh/ewwink/tinymce-combined@8.3.0/dist/tinymce-combined.min.js"></script>
+```
+
+## Version
+
+included JS and Plugin file in `tinymce-combined-standard`
+- tinymce.min.js
+- themes/silver/theme.min.js
+- models/dom/model.min.js
+- icons/default/icons.min.js
+- advlist
+- anchor
+- autolink
+- charmap
+- code
+- fullscreen
+- image
+- link
+- lists
+- media
+- preview
+- searchreplace
+- table
+- visualblocks
+- wordcount
+- 
+
+  
+<img  width="931"  height="615"  alt="TinyMCE Combined"  src="https://github.com/user-attachments/assets/f9d3b3f5-b5af-4590-a0e0-b28a0842d3e7" />
---
+++ tinymce-combined-updater.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+TinyMCE and Plugins Combined into one js file
+https://github.com/ewwink/tinymce-combined
+"""
+import os, requests, json
+from packaging.version import Version
+
+
+def set_github_env_var(name, value):
+    env_file = os.getenv("GITHUB_ENV")
+    with open(env_file, "a") as f:
+        # Note: A newline character '\n' is important for correct formatting
+        f.write(f"{name}={value}\n")
+
+
+standard_js_files = [
+    "/tinymce.min.js",
+    "/themes/silver/theme.min.js",
+    "/models/dom/model.min.js",
+    "/icons/default/icons.min.js",
+    "/plugins/advlist/plugin.min.js",
+    "/plugins/autolink/plugin.min.js",
+    "/plugins/lists/plugin.min.js",
+    "/plugins/link/plugin.min.js",
+    "/plugins/image/plugin.min.js",
+    "/plugins/charmap/plugin.min.js",
+    "/plugins/preview/plugin.min.js",
+    "/plugins/anchor/plugin.min.js",
+    "/plugins/searchreplace/plugin.min.js",
+    "/plugins/visualblocks/plugin.min.js",
+    "/plugins/code/plugin.min.js",
+    "/plugins/fullscreen/plugin.min.js",
+    "/plugins/media/plugin.min.js",
+    "/plugins/table/plugin.min.js",
+    "/plugins/wordcount/plugin.min.js",
+]
+
+standard_css_files = [
+    "/skins/ui/oxide/skin.min.css",
+    "/skins/ui/oxide/content.min.css",
+    "/skins/content/default/content.min.css",
+]
+
+full_js_file = []
+full_css_file = []
+
+base_url = "https://cdn.jsdelivr.net/npm/tinymce@"
+version_url = "https://cdn.jsdelivr.net/npm/tinymce@latest/package.json"
+
+os.makedirs("dist/skins/ui/oxide", exist_ok=True)
+os.makedirs("dist/skins/content/default", exist_ok=True)
+
+with open("version.txt", "r") as f:
+    github_version = f.read()
+    print(f"TinyMCE Github version: {github_version}")
+
+content = requests.get(version_url, timeout=10)
+tinymce_info = json.loads(content.text)
+jsdelivr_version = tinymce_info["version"]
+print(f"TinyMCE Jsdelivr version: {jsdelivr_version}")
+
+if Version(jsdelivr_version) > Version(github_version):
+    set_github_env_var("tinymce_version", jsdelivr_version)
+    set_github_env_var("tinymce_new_version", "true")
+    print(f"Updating TinyMCE...")
+    js_list = []
+    for path in standard_js_files:
+        url = f"{base_url}{jsdelivr_version}{path}"
+        print(f"Dowloading {url}..")
+        js_content = requests.get(url).text
+        js_list.append(js_content.strip())
+
+    with open("dist/tinymce-combined-standard.min.js", "w", encoding="UTF-8") as f:
+        js_combined = "\n\n".join(js_list)
+        f.write(js_combined)
+
+    for path in standard_css_files:
+        url = url = f"{base_url}{jsdelivr_version}{path}"
+        print(f"Dowloading {url}..")
+        css_content = requests.get(url).text
+        with open("dist" + path, "w", encoding="UTF-8") as f:
+            f.write(css_content)
+
+    with open("version.txt", "w", encoding="UTF-8") as f:
+        f.write(jsdelivr_version)
+
+    with open("README.md", "r") as f:
+        readme = f.read()
+        readme = readme.replace(github_version, jsdelivr_version)
+
+    with open("README.md", "r") as f:
+        f.write(readme)
+
+    print(f"TinyMCE Updated.")
+else:
+    set_github_env_var("tinymce_new_version", "false")
+    print(f"TinyMCE up to date.")
